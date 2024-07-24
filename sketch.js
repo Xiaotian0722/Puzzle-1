455fa0c260f3dc5057371e0d7ff48cee9b81f114
Xiaotian0722 <xt.0722@gmail.com>
2024-07-24 18:48:00 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,185 @@
+let state = 'start'; // states: 'start', 'game', 'success'
+let img, puz;
+let pieces = [];
+let board = [];
+let pieceSize;
+let draggingPiece = null;
+let offsetX, offsetY;
+let nextPageURL = 'https://editor.p5js.org/xt.ma0722/sketches/TjD6pFy9G';
+let startButton, nextButton, cueButton;
+let showCue = false;
+
+function preload() {
+  img = loadImage('pic/bg.webp');
+  puz = loadImage('pic/puzzle.jpg');
+}
+
+function setup() {
+  createCanvas(800, 600);
+  pieceSize = width / 3; // adjust the scale of every piece
+
+  for (let i = 0; i < 4; i++) {
+    pieces.push(new Piece(i % 2, Math.floor(i / 2)));
+    board.push(null);
+  }
+
+  startButton = createButton('Start');
+  startButton.position(width / 2 - 50, height / 2 + 100);
+  startButton.size(100, 50);
+  startButton.mousePressed(startGame);
+
+  nextButton = createButton('Next🧩');
+  nextButton.position(width / 2 - 50, height - 75);
+  nextButton.size(100, 50);
+  nextButton.mousePressed(goToNextPage);
+  nextButton.hide(); // Initially hide the next button
+
+  cueButton = createButton('Cue');
+  cueButton.position(width - 100, 50);
+  cueButton.size(50, 25);
+  cueButton.mousePressed(toggleCue);
+  cueButton.hide(); // Initially hide the cue button
+}
+
+function startGame() {
+  state = 'game';
+  startButton.hide();
+  cueButton.show();
+}
+
+function goToNextPage() {
+  window.location.href = nextPageURL;
+}
+
+function toggleCue() {
+  showCue = !showCue;
+}
+
+function draw() {
+  background(255);
+  if (state === 'start') {
+    image(img, 25, height / 2, 150, 300);
+    textSize(24);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    text('Welcome to Mindful Mosaics!', width / 2, height / 2 - 125);
+    text('Help Anixety put every puzzle piece into right place!', width / 2, height / 2 - 50);
+    text('Drag the pieces and release with mouse.', width / 2, height / 2 + 25);
+  } else if (state === 'game') {
+    if (showCue) {
+      image(puz, 250, 0, 300, 300);
+    }
+    drawBoard();
+    drawPieces();
+  } else if (state === 'success') {
+    image(puz, 250, 0, 300, 300);
+    textSize(24);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    text('You found one of the lost pieces！', width / 2, height / 2 + 100);
+    nextButton.show(); // Show the next button
+    cueButton.hide(); // Hide the cue button
+  }
+}
+
+function drawBoard() {
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 2; j++) {
+      noFill();
+      stroke(0);
+      rect(i * pieceSize, j * pieceSize, pieceSize, pieceSize);
+    }
+  }
+}
+
+function drawPieces() {
+  for (let piece of pieces) {
+    if (piece !== draggingPiece) {
+      piece.show();
+    }
+  }
+  if (draggingPiece) {
+    draggingPiece.show(mouseX - offsetX, mouseY - offsetY);
+  }
+}
+
+function mousePressed() {
+  if (state === 'game') {
+    for (let piece of pieces) {
+      if (piece.contains(mouseX, mouseY)) {
+        draggingPiece = piece;
+        offsetX = mouseX - piece.x;
+        offsetY = mouseY - piece.y;
+
+        // clear current location info of the dragged piece
+        let x = Math.floor(piece.x / pieceSize);
+        let y = Math.floor(piece.y / pieceSize);
+        let index = x + y * 2;
+        if (x >= 0 && x < 2 && y >= 0 && y < 2 && board[index] === piece) {
+          board[index] = null;
+        }
+
+        break;
+      }
+    }
+  }
+}
+
+function mouseReleased() {
+  if (draggingPiece) {
+    let x = Math.floor(mouseX / pieceSize);
+    let y = Math.floor(mouseY / pieceSize);
+    let index = x + y * 2;
+
+    if (x >= 0 && x < 2 && y >= 0 && y < 2 && !board[index]) {
+      draggingPiece.snap(x, y);
+      board[index] = draggingPiece;
+    } else {
+      draggingPiece.reset();
+    }
+
+    draggingPiece = null;
+
+    if (checkSuccess()) {
+      state = 'success';
+    }
+  }
+}
+
+function checkSuccess() {
+  for (let i = 0; i < 4; i++) {
+    if (!board[i] || board[i].index !== i) {
+      return false;
+    }
+  }
+  return true;
+}
+
+class Piece {
+  constructor(i, j) {
+    this.correctX = i * pieceSize;
+    this.correctY = j * pieceSize;
+    this.index = i + j * 2;
+    this.reset();
+  }
+
+  contains(px, py) {
+    return px > this.x && px < this.x + pieceSize && py > this.y && py < this.y + pieceSize;
+  }
+
+  show(nx = this.x, ny = this.y) {
+    image(puz, nx, ny, pieceSize, pieceSize, this.correctX, this.correctY, pieceSize, pieceSize);
+    this.x = nx;
+    this.y = ny;
+  }
+
+  snap(i, j) {
+    this.x = i * pieceSize;
+    this.y = j * pieceSize;
+  }
+
+  reset() {
+    this.x = random(width / 2, width - pieceSize);
+    this.y = random(0, height - pieceSize);
+  }
+}
